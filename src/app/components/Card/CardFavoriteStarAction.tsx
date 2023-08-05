81f8e8b8502addc9eff2560bdeb3751db8f2a9e1
will <wlymes@gmail.com>
2023-08-05 15:27:40 -0300

    feat: favorite star card

--- src/app/components/Card/CardFavoriteStarAction.tsx
+++ src/app/components/Card/CardFavoriteStarAction.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import useProjectStore from '@/store';
-import { Fragment, useState } from 'react';
+import { Fragment, useCallback, useState } from 'react';
 import { RiStarSFill, RiStarSLine } from 'react-icons/ri';
 
 type CardFavoriteStarActionProps = {
@@ -10,38 +10,33 @@ type CardFavoriteStarActionProps = {
 
 export const CardFavoriteStarAction = ({ id }: CardFavoriteStarActionProps) => {
     const [isPulsing, setIsPulsing] = useState(false);
+    const { favoritedProjects, addToFavorite } = useProjectStore()
 
-    const {
-        states: { favoritedProjetcs, },
-        actions: { addToFavorite, }
-    } = useProjectStore()
-
-
-    const projectDetails = favoritedProjetcs.projectsDetails.find((project) => project.id === id);
+    const projectDetails = favoritedProjects.projectsDetails.find((project) => project.id === id);
     const isFavorite = !!projectDetails;
     const count = projectDetails ? projectDetails.count : 0
 
-    const handleFavoriteToogle = (id: string) => {
+    const handleFavorite = useCallback((id: string) => {
         addToFavorite(id);
         setIsPulsing(true);
         setTimeout(() => setIsPulsing(false), 100);
-    }
+    }, [addToFavorite]);
 
     return (
-        <section>
+        <Fragment>
             <span
-                onClick={() => handleFavoriteToogle(id)}
-                className='absolute top-2 right-2 flex items-center justify-center z-50'
+                onClick={() => handleFavorite(id)}
+                className='absolute top-2 right-2 flex items-center justify-center z-50 cursor-pointer'
             >
                 {isFavorite ? (
                     <Fragment>
-                        <p className='text-xs text-yellow-400 select-none pr-1'>{count}</p>
+                        <p className='text-xs font-bold text-center text-yellow-400 select-none mr-2'>{count}</p>
                         <RiStarSFill className={`${isPulsing ? 'h-6 w-6' : 'w-5 h-5'} transition-all text-yellow-400 bg-white rounded-full`} />
                     </Fragment>
                 ) : (
                     <RiStarSLine className={`${isPulsing ? 'h-6 w-6' : 'w-5 h-5'} transition-all text-yellow-400 bg-white rounded-full`} />
                 )}
             </span>
-        </section>
+        </Fragment>
     );
 };
